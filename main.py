f702307fe6d8430229b2d35821036428b3361c82
Alexey Sherstobitov <asherstobitov@mail.ru>
2023-03-15 00:47:33 +0500

    Add page title and icon

--- main.py
+++ main.py
@@ -179,7 +179,7 @@ def show_metric_table(metrics: pd.DataFrame):
 config.dataFrameSerialization = "arrow"
 
 st.set_page_config(page_title='Исследование результатов опроса клиентов компании МегаФон',
-                   page_icon='chart', layout='wide')
+                   page_icon='bar-chart', layout='wide')
 # hide_menu_button()
 remove_blank_space()
 
