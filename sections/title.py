7583d64b69cbfd3221b755f7a17127ee144f7706
alexeysher <asherstobitov@mail.ru>
2025-03-12 17:21:31 +0200

    Interactive presentation updated

--- sections/title.py
+++ sections/title.py
@@ -4,16 +4,10 @@ from auxiliary import set_text_style, MegafonColors
 st.markdown(
     f'''
     <p style="color: {MegafonColors.brandPurple}; font-size: 5rem;">
-        <br>Research of MegaFon customer success survey
+        <br>Research of MegaFon customer success survey<br>
+    </p>
+    <p style="color: {MegafonColors.brandGreen}; font-size: 4rem;">
+        Author: <b>Alexey Sherstobitov</b>
     </p>
     ''', unsafe_allow_html=True
 )
-
-# plain_text = "Research of MegaFon customer success survey"
-# font_formatted_text = f'**{set_text_style(plain_text, font_size=80, color=MegafonColors.brandPurple)}**'
-# st.markdown(font_formatted_text, unsafe_allow_html=True)
-# font_formatted_text = set_text_style('&nbsp;', font_size=24)
-# st.markdown(font_formatted_text, unsafe_allow_html=True)
-# author = set_text_style('Author: ', font_size=48, color=MegafonColors.brandGreen, tag='span') + \
-#          set_text_style('**Alexey Sherstobitov**', font_size=48, color=MegafonColors.brandGreen, tag='span')
-# st.markdown(author, unsafe_allow_html=True)
