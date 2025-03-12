96382479a63a5eb466cffca25f771fe6f05183ba
alexeysher <asherstobitov@mail.ru>
2025-03-12 17:22:33 +0200

    Interactive presentation updated

--- sections/title.py
+++ sections/title.py
@@ -4,10 +4,10 @@ from auxiliary import set_text_style, MegafonColors
 st.markdown(
     f'''
     <p style="color: {MegafonColors.brandPurple}; font-size: 5rem;">
-        <br>Research of MegaFon customer success survey<br>
+        <br><b>Research of MegaFon customer success survey</b><br>
     </p>
     <p style="color: {MegafonColors.brandGreen}; font-size: 4rem;">
-        Author: <b>Alexey Sherstobitov</b>
+        <br>Author: <b>Alexey Sherstobitov</b>
     </p>
     ''', unsafe_allow_html=True
 )
